8ab75bd7b9a35f78496f28eb7fe0dbacfd261b7f
Sara Alamillo <saraalamillo93@gmail.com>
2019-07-04 16:23:40 +0200

    Home funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CabeceraComponent } from "./cabecera/cabecera.component";
-import { ContenidoComponent } from "./contenido/contenido.component";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
